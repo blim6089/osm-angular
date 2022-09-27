b0b5826ed801b2df6a4cc85db11fe73a4eeb01c4
blim6089 <blim6089@gmail.com>
2022-09-27 12:19:58 -0400

    fix

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-const API_URL = 'http://localhost:4200/ads';
+const API_URL = 'https://private-a6bca-citadu.apiary-mock.com/ads';
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
+  headerOptions = {
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': '*',
+    }),
+  };
 
   public get(): Observable<any> {
-    return this.http.get(API_URL).pipe(map((res) => res));
+    return this.http.get(API_URL, this.headerOptions).pipe(map((res) => res));
   }
 }
+// /api/users
